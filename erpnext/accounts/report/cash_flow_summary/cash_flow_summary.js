694e56b046f46d1f437da95ea759cc9986500534
O. Oda <ode.to.lan@gmail.com>
2021-11-15 11:39:30 +0800

    AP updates

--- erpnext/accounts/report/cash_flow_summary/cash_flow_summary.js
+++ erpnext/accounts/report/cash_flow_summary/cash_flow_summary.js
@@ -1,9 +1,25 @@
-// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// Copyright (c) 2021, Code Disruptors, Inc. and contributors
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Cash Flow Summary"] = {
-	"filters": [
+frappe.require("assets/erpnext/js/financial_statements.js", function() {
+	frappe.query_reports["Cash Flow"] = $.extend({},
+		erpnext.financial_statements);
 
-	]
-};
+	erpnext.utils.add_dimensions('Cash Flow', 10);
+
+	// The last item in the array is the definition for Presentation Currency
+	// filter. It won't be used in cash flow for now so we pop it. Please take
+	// of this if you are working here.
+
+	frappe.query_reports["Cash Flow"]["filters"].splice(8, 1);
+
+	frappe.query_reports["Cash Flow"]["filters"].push(
+		{
+			"fieldname": "include_default_book_entries",
+			"label": __("Include Default Book Entries"),
+			"fieldtype": "Check",
+			"default": 1
+		}
+	);
+});
